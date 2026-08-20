b91aa2b5c9431e94954297b0db1b73605e0a3c00
koirdev <xdelta466@gmail.com>
2026-08-20 17:42:45 +0000

    fucked up with gitignore

--- CMakeFiles/my_raylib_game.dir/src/main.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/my_raylib_game.dir/src/main.c.o: \
- /home/koirdev/Desktop/kdEngine_raylib/src/main.c \
- /usr/include/stdc-predef.h \
- /home/koirdev/Desktop/kdEngine_raylib/_deps/raylib-src/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/14/include/stdarg.h \
- /usr/lib/gcc/x86_64-linux-gnu/14/include/stdbool.h
